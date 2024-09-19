abb1cfebb506f98defdf99e1abc9b29ab78ccc41
GaliSuarez <2030944e@umich.mx>
2024-09-19 12:41:08 -0600

    Carga Tarea V1.0
    En esta carga se sube la primera versión de la tarea de interés simple

---
+++ formulasInteresSimple.R
@@ -0,0 +1,12 @@
+# Se presentan las funciones de interés simple
+# Autor: Karla Galilea Suárez Esquivel
+# V 1.0: 19 de septiembre de 2024
+
+# Valor futuro con interes simple
+
+valorFinalSimple=function(VA,r,t){
+                                  xSalida=VA*(1+(r*t))
+                                  return(xSalida)
+                                  }
+
+# Valor actual con Interés simple
